1f8b7342cb9cc09c063b7439dc5dd0900896b230
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-03 17:25:51 +0200

    feat: exercise

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 Dovete stampare la lista di spesa in pagina utilizzando ciclo while.
 Rifacciamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+
+### Svolgimento
+
+- Creo un array contenente i prodotti della lista
+- Dichiaro una variabile di appoggio per catturare la classe 'ul'
+- Inizializzo una variabile contatore
+- Creo un ciclo while che generi gli elementi della lista finchè non stato inserito ogni elemento dell'array
+- Stampo gli elementi in pagina
--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 // Test
 console.log('Hello World!');
+
+// Creo un array contenente i prodotti della lista
+const shoppingList = ["pane", "pasta", "olio", "farina", "sale", "latte", "pomodoro"]; // array
+console.log(shoppingList);
+
+// Dichiaro una variabile di appoggio per individuare la classe <ul>
+let listElem = document.querySelector(".shopping-list"); // object
+console.log(listElem);
+
+// Inizializzo una variabile contatore
+let i = 0; // number
+
+// Creo un ciclo while che generi gli elementi della lista finchè non è terminato l'array
+while (i < shoppingList.length) {
+    // Prendo l'elemento dell'array e lo memorizzo in una variabile di appoggio
+    const curItem = shoppingList[i]; // string
+
+    // Creo l'elemento (oggetto) <li> della lista e metto all'interno l'elemento dell'array
+    const listItemElem = document.createElement("li"); // object
+    listItemElem.innerHTML = curItem;
+    console.log(listItemElem);
+
+    // Aggiungo l'elemento (oggetto) <li> alla lista in modo da stamparlo in pagina
+    listElem.append(listItemElem);
+
+    // Incremento il contatore
+    i++;
+}
